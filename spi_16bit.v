8d3e5733a62b994ac661385b333b394dc2df99bf
liwy <10162100162@stu.ecnu.edu.cn>
2020-09-14 14:03:42 +0800

    first_commit

--- spi_16bit.v
+++ spi_16bit.v
@@ -95,7 +95,7 @@ begin
         if(rx_buf[15] != 1)     // 最高位为1时 主站只接收数据，不发送数据，所以不置位rx_flag从而不发送曼码
         begin
             rx_flag <= 1;   // cs拉高时接收完成
-
+            tx_buf <= 0;    // 发送缓冲区清0
         end
 
     end
